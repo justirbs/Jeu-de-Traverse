f6f95587087a51984f35eef0f02f389a1b6ad6b8
Sylvain JALBERT <moa.sjsj@gmail.com>
2020-04-20 10:03:56 +0200

    Ajout des commentaires dans main

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/*!
+ * \file main.c
+ *
+ * \author Vincent Donney et Justine Ribas <ribasjusti@eisti.eu>
+ * \version 0.1
+ * \date 20 avril 2020
+ *
+ * \brief fonction princpale
+ *
+ *
+ */
+
+// Inclusion des librairies
+
+
+//Code de la fonction
+
+int int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
